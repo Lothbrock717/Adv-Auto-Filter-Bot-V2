80380371d67527b68c10cb2a8aca09f9df8752c0
kevinnadar264 <76389377+kevinnadar264@users.noreply.github.com>
2021-05-06 23:32:39 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -109,8 +109,7 @@ async def auto_filter(bot, update):
     else:
         await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"<b>No Movies Found</b>\n\n<b>If U didn't get any Movie , Tag @admin with your Movie Name </b>",
-                reply_markup=reply_markup,
+                text=f"<b>No Movies Found</b>\n\n<b>If U didn't get any Movie , Tag @admin with your Movie Name </b>",                
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
