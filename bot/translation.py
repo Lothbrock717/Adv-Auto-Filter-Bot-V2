af85d0f686381db4dbbed686e138760c9b90acaf
kevinnadar264 <76389377+kevinnadar264@users.noreply.github.com>
2021-05-14 00:36:25 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,10 +5,15 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<b> I'm Busy . Don't Waste Your Time @T2Link</b>   """    
+<b>I'm Busy . Don't Waste Your Time @T2Links</b>   """    
     
     HELP_TEXT = """
-<b> Already Said , Don't WasTe Your Time</b>            
+<b> Already Said , Don't Waste Your Time</b> 
+
+
+
+
+<b>@T2Links</b>            
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Anonymous</code>
