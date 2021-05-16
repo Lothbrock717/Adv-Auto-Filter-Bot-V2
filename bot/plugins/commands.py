efddc8eb4a6d1039a91a72186f39182940196a18
kevinnadar264 <76389377+kevinnadar264@users.noreply.github.com>
2021-05-16 15:34:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<b>@T2Links </b>" + "<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<b>@T2Links.</b>" + "<code>" + file_name + "</code>")
         
         if file_type == "document":
         
