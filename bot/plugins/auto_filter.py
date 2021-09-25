8ffc0c61e8a8d59b3a91335a5cb8de1e49ee57d0
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2021-09-25 22:22:11 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -165,7 +165,7 @@ async def auto_filter(bot, update):
             InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
         ])
         
-        result[0].append([ InlineKeyboardButton(f"🎗 Join Our Channel 🎗", url="https://t.me/tamil_latest_films") ])
+        result[0].append([ InlineKeyboardButton(f"🎗 Join Our Channel 🎗", url="https://t.me/Ajeditz42") ])
         # if show_invite is True Append invite link buttons
         if show_invite:
             
